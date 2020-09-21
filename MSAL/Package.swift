71a06d088d15ebe3ca39672787ccffd7a1acfc09
Jakub Gencer <jakub.gencer@allaboutapps.at>
2020-09-21 15:07:10 +0200

    Edit Package.swift

--- MSAL/Package.swift
+++ MSAL/Package.swift
@@ -1,1 +1,17 @@
 
+import PackageDescription
+
+let package = Package(
+    name: "MSAL",
+    products: [
+        .library(
+            name: "microsoft-authentication-library-for-objc",
+            targets: ["MSAL"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MSAL",
+            url: "TODO",
+            checksum: "TODO")
+    ]
+)
