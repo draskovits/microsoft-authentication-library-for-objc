a0ab25fc1b08aff74d2778aabccf6536299bad28
Stefan Draskovits <stefan.draskovits@me.com>
2020-12-15 08:20:18 +0100

    user local reference instead of URL

--- Package.swift
+++ Package.swift
@@ -12,9 +12,6 @@ let package = Package(
             targets: ["MSAL"]),
     ],
     targets: [
-        .binaryTarget(
-            name: "MSAL",
-            url: "https://github.com/draskovits/microsoft-authentication-library-for-objc/releases/download/1.1.9-spm/MSAL.xcframework.zip",
-            checksum: "10d64df56adb677cc075dc06a3e71fa3dfada2adc13bdec4cdb0abca3eff27ce")
+        .binaryTarget(name: "MSAL", path: "Builds/MSAL.xcframework")
     ]
 )
